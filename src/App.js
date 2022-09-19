e0ef4ed9c1b352e409abb1f56e49e9eb5d7859b2
Scott Wilson <swrs@hotmail.co.uk>
2022-09-19 11:32:33 +0100

    created fetch template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+    async function FetchData() {
+      const response = await fetch(`https://api.fbi.gov/wanted/v1/list`);
+      const data = await response.json();
+      setApiData(data);
+    }
+    FetchData();
+  }, []);
+  console.log(apiData);
+  return <div className="App"></div>;
 }
 
 export default App;
